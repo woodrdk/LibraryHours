7f3c1b3d86d5e112632578d0289f1a3bdc990138
Robert Wood Jr <rdrwood@gmail.com>
2019-01-31 14:38:01 -0800

    added code to get hours

--- LibraryHours/Main.cpp
+++ LibraryHours/Main.cpp
@@ -24,7 +24,7 @@ using namespace std;
 
 enum class Day { Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday, InvalidDay };
 Day getDay(string day);
-string libraryHours(string today);
+string libraryHours(Day today);
 
 // function prototypes go here
 int main() {
@@ -35,43 +35,78 @@ int main() {
 	cout << "Your input: " << day << endl;
 	Day today = getDay(day);
 	cout << static_cast<int>(today) << endl;
-	//cout << libraryHours(today) << endl;
+	cout << libraryHours(today) << endl;
 	system("pause");
 	return 0;
 }
 
 // function definitions go here
+
 Day getDay(string day) {
-	if (tolower(day[0]) == 'm') {
+	switch (tolower(day[0])){
+	case 'm':
 		return Day::Monday;
-	}
-	else if (tolower(day[0]) == 't') {
-		if (tolower(day[1]) == 'u') {
-			return Day::Tuesday;
-		}
-		else {
-			return Day::Thursday;
-		}
-	}
-	else if (tolower(day[0]) == 'w') {
+	case 't':
+		return tolower(day[1]) == 'u' ? Day::Tuesday : Day::Thursday;
+	case 'w':
 		return Day::Wednesday;
-	}
-	else if (tolower(day[0]) == 'f') {
-		return Day::Friday;
-	}
-	else if (tolower(day[0]) == 's') {
-		if (tolower(day[1] == 'a')) {
-			return Day::Saturday;
-		}
-		else {
-			return Day::Sunday;
-		}
-	}
-	else {
+	case 'f':
+		return 	Day::Friday;
+	case 's':
+		return tolower(day[1]) == 'a' ? Day::Saturday : Day::Sunday;
+	default: 
 		return Day::InvalidDay;
 	}
 }
 
-//string libraryHours(today) {
-//
+//Day getDay(string day) {
+//	if (tolower(day[0]) == 'm') {
+//		return Day::Monday;
+//	}
+//	else if (tolower(day[0]) == 't') {
+//		if (tolower(day[1]) == 'u') {
+//			return Day::Tuesday;
+//		}
+//		else {
+//			return Day::Thursday;
+//		}
+//	}
+//	else if (tolower(day[0]) == 'w') {
+//		return Day::Wednesday;
+//	}
+//	else if (tolower(day[0]) == 'f') {
+//		return Day::Friday;
+//	}
+//	else if (tolower(day[0]) == 's') {
+//		if (tolower(day[1] == 'a')) {
+//			return Day::Saturday;
+//		}
+//		else {
+//			return Day::Sunday;
+//		}
+//	}
+//	else {
+//		return Day::InvalidDay;
+//	}
 //}
+
+string libraryHours(Day today) {
+	switch (today) {
+		case Day::Monday:
+		case Day::Tuesday:
+		case Day::Wednesday:
+			return  "10AM - 6PM";
+		case Day::Thursday:
+			return "10AM - 8PM";
+		case Day::Friday:
+			return "10AM - 8PM";
+		case Day::Saturday:
+		case Day::Sunday:
+			return "10AM - 9PM";
+		case Day::InvalidDay:
+			return "not a day";
+
+	}
+
+	return "jgg";
+}
